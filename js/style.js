21d759168e2403926e61ce955bea583b8c6528ec
Gleb952 <lomakin.plex@mail.ru>
2022-10-15 21:21:28 +0300

    getElementById

--- js/style.js
+++ js/style.js
@@ -1,23 +1,51 @@
-var toDoList = [];
+window.onload = function () {
+    var toDoList = [];
+    if (localStorage.getItem('todo') != undefined) {
 
-document.getElementById('buttonClick').onclick = function buttonToDo() {
-
-
-    var buttonClick = document.getElementById('input').value;
-    var temp = {};
-    temp.todo = buttonClick;
-    temp.check = false;
-    var i = toDoList.length;
-    toDoList[i] = temp;
-    console.log(toDoList);
+        toDoList = JSON.parse(localStorage.getItem('todo'));
+        out();
+    }
 
     function out() {
         var out = "";
         for (var key in toDoList) {
+
+            if (toDoList[key].check == true) {
+                out += '<input type="checkbox" checked>' + '<button id="buttonDel">Delete</button>'
+            } else {
+                out += '<input type="checkbox">' + '<button id="buttonDel">Delete</button>'
+            }
+
             out += toDoList[key].todo + "</br>";
+
         }
         document.getElementById("out").innerHTML = out;
     }
 
-    out();
+    document.getElementById('buttonClick').onclick = function buttonToDo() {
+        function getNewID(arr) {
+            var max = 1;
+            for (const item of arr) {
+                if (item.id > max) max = item.id
+            }
+            return max + 1;
+
+        }
+
+        var buttonClick = document.getElementById('input').value;
+        var temp = {};
+        temp.todo = buttonClick;
+        temp.check = false;
+        temp.id = getNewID(toDoList);
+        var i = toDoList.length;
+        toDoList[i] = temp;
+        console.log(toDoList);
+
+
+        out();
+
+
+    }
+
+
 }
